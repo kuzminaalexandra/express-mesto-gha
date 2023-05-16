d4fee391a45f67750c379fc3c3c52d173c0266f0
kuzminaalexandra <113555701+kuzminaalexandra@users.noreply.github.com>
2023-05-16 12:15:41 +0500

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,14 +7,22 @@ module.exports.auth = (req, res, next) => {
   const token = req.cookies.jwt;
 
   if (!token) {
-    next(new CustomError('Авторизуйтесь!', StatusCodes.UNAUTHORIZED));
+    return next(new CustomError('Необходима авторизация', StatusCodes.UNAUTHORIZED));
   }
 
+  let payload;
+
   try {
-    const payload = jwt.verify(token, 'secret1111');
-    req.user = payload;
-    next();
-  } catch (error) {
-    next(new CustomError('Авторизуйтесь!', StatusCodes.UNAUTHORIZED));
+    payload = jwt.verify(token, 'secret1111');
+  } catch (err) {
+    return next(new CustomError('Необходима авторизация', StatusCodes.UNAUTHORIZED));
   }
+
+  if (!payload) {
+    return next(new CustomError('Необходима авторизация', StatusCodes.UNAUTHORIZED));
+  }
+
+  req.user = payload;
+
+  return next();
 };
